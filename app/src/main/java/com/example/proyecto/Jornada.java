a30321d5fb75109e2c281a63a0c4f5a63f87bb6a
csanchezmo <csanchezmoyano@gmail.com>
2024-10-30 13:51:37 +0100

    Creacion de clases y vaariables que faltaban

---
+++ app/src/main/java/com/example/proyecto/Jornada.java
@@ -0,0 +1,10 @@
+package com.example.proyecto;
+
+import java.util.ArrayList;
+
+//Clase que contiene la informacion de cada jugador en cada jornada.
+public class Jornada {
+    private ArrayList<Jugador> jugadores;
+    private int puntuacion;
+    //private int nJornada;
+}
--- app/src/main/java/com/example/proyecto/Usuario.java
+++ app/src/main/java/com/example/proyecto/Usuario.java
@@ -1,6 +1,23 @@
 package com.example.proyecto;
 
 
+import android.widget.ImageView;
+
+import java.util.ArrayList;
+
 /*Clase que representa a los usuarios de la aplicación.*/
 public class Usuario {
+    private String nickname;
+    private ImageView avatar;
+    private boolean esAdmin = false;
+    private int puntuacionTotal;
+    private int saldo;
+    private ArrayList<Jugador> jugadores;
+    private int nJugadores = jugadores.size();
+    private ArrayList<Jornada> jornadas;
+    private int posicion;
+
+
+
+
 }
